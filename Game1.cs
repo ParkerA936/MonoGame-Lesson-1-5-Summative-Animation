de44edcaea2658b47ca80d3622d42dff890c2bec
Parker Arnett <362581936@GoTVDSB.ca>
2026-05-04 13:12:42 -0400

    Blab blah blah

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MonoGame_Lesson_1_5_Summative_Animation
 {
@@ -16,15 +16,16 @@ namespace MonoGame_Lesson_1_5_Summative_Animation
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D starBackTexture, explosionBackTexture;
+        Texture2D starBackTexture, shipTexture;
         SpriteFont skyText;
-        Rectangle window,starBackRect, explosionBackRect;
+        Rectangle window,starBackRect, shipRect;
         Screen screen;
         MouseState mouseState, prevMouseState;
         Vector2 textLocation, textSize, prevTextSize;
         float textScale = 1f;
         float xTextOffset;
-
+        SoundEffect themeSound;
+        SoundEffectInstance instantThemeSound;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -43,8 +44,9 @@ namespace MonoGame_Lesson_1_5_Summative_Animation
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
             textLocation = new Vector2(150, 700);
+           
             starBackRect = new Rectangle(0, 0, window.Width, window.Height);
-            explosionBackRect = new Rectangle(0, 0, window.Width, window.Height);
+            shipRect = new Rectangle(0, 250, 340, 300);
             xTextOffset = 0f;
             base.Initialize();
             textSize = skyText.MeasureString("Welcome to Parkers Animation\n\n Click To Continue");
@@ -57,7 +59,9 @@ namespace MonoGame_Lesson_1_5_Summative_Animation
 
             starBackTexture = Content.Load<Texture2D>("12");
             skyText = Content.Load<SpriteFont>("SkyText");
-            explosionBackTexture = Content.Load<Texture2D>("death-star");
+            shipTexture = Content.Load<Texture2D>("death_Star");
+            themeSound = Content.Load<SoundEffect>("Star Wars Main Theme (Full)");
+            instantThemeSound = themeSound.CreateInstance();
             // TODO: use this.Content to load your game content here
         }
 
@@ -82,15 +86,17 @@ namespace MonoGame_Lesson_1_5_Summative_Animation
                 this.Window.Title = textSize.X.ToString();
                 textLocation.X += (prevTextSize.X - textSize.X) / 2;
 
-
+                instantThemeSound.Play();
 
                 if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
                 {
                     screen = Screen.GamePlay;
+                    
                 }
             }
             else if (screen == Screen.GamePlay)
             {
+               instantThemeSound.Stop();
                 if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
                 {
                     screen = Screen.End;
@@ -109,12 +115,16 @@ namespace MonoGame_Lesson_1_5_Summative_Animation
             {
                 _spriteBatch.Draw(starBackTexture, starBackRect, Color.White);
                 
-                
                 _spriteBatch.DrawString(skyText, "Welcome to Parkers Animation\n\n        Click To Continue", textLocation, Color.Yellow, 0f, Vector2.Zero, textScale, SpriteEffects.None, 1f);
+            }
+            if (screen == Screen.GamePlay)
+            {
+                _spriteBatch.Draw(starBackTexture, starBackRect, Color.White);
+                _spriteBatch.Draw(shipTexture, shipRect, Color.White);
             }
             if (screen == Screen.End)
             {
-                _spriteBatch.Draw(explosionBackTexture, explosionBackRect, Color.White);
+               
                 _spriteBatch.DrawString(skyText, ("The End"), new Vector2(250, 200), Color.Yellow);
 
             }
